9bf45e25dde864b73b9bb8f28968ce0c7af38091
joshmorg1007 <eston1007@gmail.com>
2021-03-31 16:50:10 -0700

    fixed the source error

--- telegrafInit.sh
+++ telegrafInit.sh
@@ -1,10 +1,8 @@
 wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-
-
-sudo apt update
-sudo apt install telegraf
+sudo apt-get update
+sudo apt-get install telegraf
 
 sudo systemctl enable --now telegraf
 sudo systemctl start --now telegraf

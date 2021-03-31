b4d0dcb4d217a56f7892a6b3ccc104118d1443da
joshmorg1007 <eston1007@gmail.com>
2021-03-31 12:09:08 -0700

    added sources to scrip

--- telegrafInit.sh
+++ telegrafInit.sh
@@ -1,3 +1,8 @@
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+
 sudo apt update
 sudo apt install telegraf
 

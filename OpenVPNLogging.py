1643b5e8f12f0416c67eb2ba73faf5832da640aa
joshmorg1007 <eston1007@gmail.com>
2021-04-08 10:41:06 -0700

    added filtering of syslog for only pertanent info

--- OpenVPNLogging.py
+++ OpenVPNLogging.py
@@ -416,6 +416,7 @@ def concat_syslogs():
     """Concatinates all syslog files into one temp file"""
     os.system("/bin/cat /var/log/syslog.7.gz /var/log/syslog.6.gz /var/log/syslog.5.gz /var/log/syslog.4.gz /var/log/syslog.3.gz /var/log/syslog.2.gz | /bin/gunzip > " + TMP_FILE_PATH)
     os.system("/bin/cat /var/log/syslog.1 /var/log/syslog >> " + TMP_FILE_PATH)
+    os.system("cat " + TMP_FILE_PATH + "| grep ovpn-server > " + TMP_FILE_PATH)
 
 def get_con_datetime(date):
     """Converts the timestamp in syslog to miliseconds"""
